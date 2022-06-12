94ab351abc578f13f85facc659c1a35cc18fd0e0
Fakhri112 <dracozen112@gmail.com>
2022-06-12 20:33:23 +0700

    fix cors

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ const express = require('express')
 const cookieparser = require('cookie-parser');
 const cors = require('cors')
 const router = require("./utils/route");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+ const { corsOptionsDelegate } = require('./controller/cors');
 const app = express()
 const port = process.env.PORT || 5000;
 dotenv.config()
 
+
+
 app.use(cookieparser('hello'))
 app.use(express.static('public')) //middleware
 app.use(express.json())
-app.use(cors({
-// allowed site to use API
-
-}));
+app.use(cors(
+    corsOptionsDelegate
+));
 app.use(router);
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
